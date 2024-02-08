6572641afb7233650e8f5dcebf34d9e8c4e7b81c
Bakir Gracić <bakirgdev@gmail.com>
2024-02-09 00:12:19 +0100

    Update script.sh
    added internet checker, curl and dig presence verification

--- script.sh
+++ script.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
 
+# check for Internet
+if ! ping -c 1 -W 2 -q www.google.com > /dev/null 2>&1; then
+  printf "There appears to be no Internet connection. Exiting...\n" >&2
+  exit 1
+fi
+
+# verify curl and dig are installed on system
+if ! command -v curl 1> /dev/null; then
+  printf "'curl' must be installed on your system. Exiting...\n" 1>&2
+  exit 1
+fi
+if ! command -v dig 1> /dev/null; then
+  printf "'dig', part of the 'dnsutils' package, must be installed on your system. Exiting...\n" 1>&2
+  exit 1
+fi
+
 # Function to validate the URL
 validate_url(){
   if [[ $1 =~ ^http(s)?://[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}(/.*)?$ ]]; then
